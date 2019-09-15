9316cfbfb13ed224ff8ebcfd41dc8b8e48e9628e
kevin400085 <kevin400085@yahoo.com>
2019-09-15 18:10:45 -0400

    Created simple platforming demo

---
+++ Assets/Scripts/EnemySquishScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySquishScript : MonoBehaviour
+{
+    private GameObject upgradeManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        upgradeManager = GameObject.Find("UpgradeManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.gameObject.tag == "Enemy")
+        {
+            if (collision.collider.GetType() == typeof(SphereCollider))
+            {
+                Debug.Log("Hit");
+                Destroy(collision.transform.gameObject);
+                upgradeManager.GetComponent<UpgradeController>().GainDoubleJump();
+            }
+            else
+            {
+                // Receive dmg
+            }
+        } 
+    }
+}
---
+++ Assets/Scripts/UpgradeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UpgradeController : MonoBehaviour
+{
+    public bool doubleJump = false;
+    public Text notification;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GainDoubleJump()
+    {
+        doubleJump = true;
+        notification.text = "You can now double jump.";
+    }
+}
