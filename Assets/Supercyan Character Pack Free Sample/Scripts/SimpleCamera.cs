c70bd839d6cfbb4a3db4ac619027934416cf44b2
Kevin Nguyen <Kevinnguyen55@users.noreply.github.com>
2019-10-16 17:30:06 -0400

    Added Super Cyan Model and Created Simple Camera

---
+++ Assets/Supercyan Character Pack Free Sample/Scripts/SimpleCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCamera : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float smoothFactor = 1.0f;
+
+    public bool lookAtPlayer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOffset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate() {
+        Vector3 updatePosition = player.transform.position + cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, updatePosition, smoothFactor);
+
+        if (lookAtPlayer) {
+            transform.LookAt(player.transform);
+        }
+    }
+}
