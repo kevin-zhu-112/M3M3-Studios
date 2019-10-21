9afface0447f8986e9d7818ae93a2272e17950c6
Kevin Nguyen <Kevinnguyen55@users.noreply.github.com>
2019-10-20 21:04:44 -0400

    Added MainCharacter Prefab and Camera Prefab for playable char

--- Assets/Scripts/CameraFollowScript.cs
+++ Assets/Scripts/CameraFollowScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 // Base code taken from https://www.youtube.com/watch?v=LbDQHv9z-F0 and modified to include controller support
 public class CameraFollowScript : MonoBehaviour
@@ -24,6 +25,25 @@ public class CameraFollowScript : MonoBehaviour
     private float rotY = 0.0f;
     private float rotX = 0.0f;
 
+    PlayerControls controls;
+
+    private Vector2 cameraVector;
+
+    void Awake() {
+         controls = new PlayerControls();
+
+        controls.Gameplay.Camera.performed += ctx => cameraVector = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Camera.canceled += ctx => cameraVector = Vector2.zero;
+    }
+
+    public void OnEnable() {
+        controls.Enable();
+    }
+
+    public void OnDisable() {
+        controls.Disable();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,12 +57,12 @@ public class CameraFollowScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //float inputX = Input.GetAxis("RightStickHorizontal");
-        //float inputZ = Input.GetAxis("RightStickVertical");
+        float inputX = cameraVector.x;
+        float inputZ = -cameraVector.y;
         mouseX = Input.GetAxis("Mouse X");
         mouseY = Input.GetAxis("Mouse Y");
-        finalInputX = mouseX;
-        finalInputZ = mouseY;
+        finalInputX = mouseX + inputX;
+        finalInputZ = mouseY + inputZ;
 
         rotY += finalInputX * InputSensitivity * Time.deltaTime;
         rotX += finalInputZ * InputSensitivity * Time.deltaTime;
@@ -61,8 +81,10 @@ public class CameraFollowScript : MonoBehaviour
     void CameraUpdater()
     {
         Transform target = CameraFollowObj.transform;
+        //target.position = target.position + new Vector3(camDistanceXToPlayer, camDistanceYToPlayer, camDistanceZToPlayer);
 
         float step = CameraMoveSpeed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        //transform.LookAt(target.position);
     }
 }
