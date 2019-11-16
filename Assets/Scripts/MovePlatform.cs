4f5691c4ba00da79724deab7cbee4196e081d577
Kevin Nguyen <Kevinnguyen55@users.noreply.github.com>
2019-11-15 21:37:52 -0500

    Added moving nextLevel platform

---
+++ Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    GameObject platform;
+    Vector3 finalPos = new Vector3(-0.67f, -9.16f, -48.5f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Slerp(transform.position, finalPos, 0.03f);//Vector3.Lerp(startMarker.position, endMarker.position, fractionOfJourney);
+    }
+    
+}
