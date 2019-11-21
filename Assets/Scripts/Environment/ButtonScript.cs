f71dba00b5d7cf69513881211a292e1b6231c987
Akshay Karthik <akshay.karthik98@gmail.com>
2019-11-20 21:58:40 -0500

    some changes

--- Assets/Scripts/Environment/ButtonScript.cs
+++ Assets/Scripts/Environment/ButtonScript.cs
@@ -7,6 +7,9 @@ public class ButtonScript : MonoBehaviour
     private Animator m_Animator;
     private AudioSource m_Audio;
     public GameObject target;
+    private GameObject presser;
+    private bool boxPressed;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +20,33 @@ public class ButtonScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (presser == null && boxPressed)
+        {
+            m_Animator.enabled = false;
+            boxPressed = false;
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.gameObject.tag == "Player")
+        if (collision.transform.gameObject.tag == "Player" || collision.transform.gameObject.tag == "Placeable")
         {
+            if (collision.transform.gameObject.tag == "Placeable")
+            {
+                boxPressed = true;
+                presser = collision.transform.gameObject;
+            }
+            m_Animator.enabled = true;
             m_Animator.SetTrigger("Move");
             m_Audio.Play(0);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.gameObject.tag == "Player")
+        {
+            m_Animator.enabled = false;
         }
     }
 }
