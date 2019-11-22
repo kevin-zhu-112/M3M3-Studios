e2a4647ca59e56d34a13c288e48429e122d47272
Alex Booth <alexbooth08@gmail.com>
2019-11-22 11:24:03 -0500

    Fiexed pencil script

--- Assets/Scripts/AI/PencilScript.cs
+++ Assets/Scripts/AI/PencilScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pencil : MonoBehaviour
+public class PencilScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
