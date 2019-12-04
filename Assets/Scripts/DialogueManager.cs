aefb010ae9e26adc82f9653a8e597aead7a243f9
Kevin Nguyen <Kevinnguyen55@users.noreply.github.com>
2019-12-04 17:32:37 -0500

    Added inital dialogue stuff

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public Text nameText;
+    public Text dialogueText;
+    public GameObject dialogueBox;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue log) {
+        dialogueBox.SetActive(true);
+        nameText.text = log.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in log.sentences) {
+            sentences.Enqueue(sentence);
+        }
+
+        StartCoroutine(DisplayNextSentence());
+    }
+
+    IEnumerator DisplayNextSentence() {
+        while(sentences.Count != 0) {
+            string sentence = sentences.Dequeue();
+            dialogueText.text = sentence;
+            yield return new WaitForSeconds(4.0f);
+            if (sentences.Count == 0) {
+                dialogueBox.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.transform.gameObject.tag == "Player") {       
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        }
+    }
+}
