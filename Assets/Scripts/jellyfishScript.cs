3bb8b425da13267f6ced8ba1a38571674a57a7fe
Alex Booth <alexbooth08@gmail.com>
2019-12-05 12:19:52 -0500

    Added jellyfish obstacle prefab

---
+++ Assets/Scripts/jellyfishScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jellyfishScript : MonoBehaviour
+{
+    AudioSource soundEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundEffect = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+            soundEffect.Play();
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("hit");
+        }
+    }
+
+
+}
